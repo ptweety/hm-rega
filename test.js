d95cab33650403b4940e08cdaf022b57bd105870
Matthias <me@ptweety.net>
2022-11-14 21:45:11 +0100

    chore: update test.js

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 const Rega = require('./index.js');
 const rega = new Rega({
-    host: 'homematic-ccu3',
-    disableTranslation: false
+    host: 'homematic-ccu3'
+    , disableTranslation: false
+    , language: 'en'
+    , tls: true
+    , inSecure: true
 });
 
 
@@ -57,12 +60,37 @@ rega.getChannels((err, res) => {
 
 
 
+*/
+
+
+rega.getPrograms((err, res) => {
+    console.log('getPrograms', err, res);
+});
 
 rega.getRooms((err, res) => {
-    console.log(err,res);
-   // require('fs').writeFileSync('values.json', JSON.stringify(res, null, '  '));
-});*/
+    console.log('getRooms', err,res);
+});
+
+rega.getFunctions((err, res) => {
+    console.log('getFunctions', err, res);
+});
 
 rega.getVariables((err, res) => {
     console.log('getVariables', err, res);
+});
+
+rega.getChannels((err, res) => {
+    console.log('getChannels', err, res);
+});
+
+rega.getValues((err, res) => {
+    console.log('getValues', err, res);
+});
+
+rega.exec('string x = "Hello";\nWriteLine(x # " World!");', (err, output, objects) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Script Output:', output);
+    console.log('Script Objects:', objects);
 });
